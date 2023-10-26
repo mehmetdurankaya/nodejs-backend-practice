bcae6d03d88857239f9688127f20a269174aae0c
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-10-27 02:34:59 +0300

    nodemon and express modul add

---
+++ express-app/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app= express();
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+});
+app.get('/api/products',(req,res)=>{
+    res.send('ürünler listelendi');
+});
+
+app.listen(3000,()=>{
+console.log('listening on port 3000');
+});
